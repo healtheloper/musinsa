30eaa44c633fcaf0b01e6604473b6d81c78bfb9c
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-07-13 12:57:54 +0900

    style: figma's

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  grey: '#979797',
+  grey4: '#EEE',
+  grey5: '#CCC',
+  grey6: '#AAA',
+  black: '#000',
+  green: '#18A286',
+  red: '#FF0000',
+  blue: '#0078FF',
+};
+
+export default colors;
---
+++ src/constants/fonts.ts
@@ -0,0 +1,66 @@
+export const fontSizes = {
+  xsmall: '11px',
+  small: '12px',
+  medium: '14px',
+  large: '16px',
+};
+
+export const fontWeights = {
+  lighter: 400,
+  normal: 500,
+  bold: 700,
+};
+
+export const fontLineHeights = {
+  lh1: '12px',
+  lh2: '16px',
+  lh3: '18px',
+  lh4: '21px',
+  lh5: '24px',
+};
+
+export const fontFamilys = {
+  appleGothic: 'Apple SD Gothic Neo',
+  musinsaOTF: 'MUSINSA OTF',
+};
+
+export const typhographys = {
+  h5: {
+    // 상품명
+    fontSize: 'medium',
+    fontWeight: 'bolder',
+    lineHeight: 'lh3',
+  },
+  h6: {
+    // 브랜드명
+    fontSize: 'xsmall',
+    fontWeight: 'lighter',
+    lineHeight: 'lh2',
+  },
+  body3: {
+    // 할인전 가격
+    fontSize: 'xsmall',
+    fontWeight: 'normal',
+    lineHeight: 'lh1',
+  },
+  body2: {
+    // 필터로 생긴 버튼, 레이블
+    fontSize: 'small',
+    fontWeight: 'lighter',
+    lineHeight: 'lh3',
+  },
+  body1: {
+    // 가격, 할인율
+    fontSize: 'large',
+    fontWeight: 'normal',
+    lineHeight: 'lh5',
+  },
+  placeholder: {
+    // 검색창 placeholder
+    fontSize: 'large',
+    fontWeight: 'lighter',
+    lineHeight: 'lh5',
+  },
+};
+
+// MUSINSA OTF - 가격, 할인율, 할인전가격, 레이블
